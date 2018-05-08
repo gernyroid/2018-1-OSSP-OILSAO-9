98dbe96e5ed4e2576391b287197b81a35b96e997
이원경 <eglantina@naver.com>
2018-05-08 18:45:46 +0900

    Add files via upload

---
+++ item.cpp
@@ -0,0 +1,32 @@
+#include "game_states.h"
+using namespace std;
+
+void menu()
+{
+	
+}
+
+
+void main_game(int selector, int mode)
+{
+
+// 아이템 기능
+item_no = item_make();
+
+  if(item_no = 1)// 라이프 증가
+  {
+   if(life <=3){
+      life ++;
+      if(life = 3) apply_surface(580, 20, heart, screen);
+      else if(life = 2) apply_surface(540, 20, heart, screen);
+      else if(life = 1) apply_surface(580, 20, heart, screen);
+}
+     
+  }
+}
+
+
+
+
+
+
